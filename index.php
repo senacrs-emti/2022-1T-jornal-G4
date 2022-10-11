869fcde33e7931a3972e02bdbea378be46d43c39
ErickNetto <102562548+ErickNetto@users.noreply.github.com>
2022-10-11 17:55:53 -0300

    commit pequeno
    só botei estrutura ali e adicionei uma imagem pra estruturar depois

--- index.php
+++ index.php
@@ -3,7 +3,18 @@
 include_once './includes/_header.php'
 //parte do conteudo da página
 ?>
-
+<main>
+    <div class='row'>
+        <div class="col">
+           <section style="">
+                <img src="./assets/img/Rectangle 13.png" style="height:200px" alt="">
+           </section>
+        
+       
+            
+        </div>
+    </div>
+</main>
 <?php
 //include do aside
 include_once './includes/_aside.php';
