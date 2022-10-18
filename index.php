6fdde12e19a4e9b9ff4822156a223bae06e7379b
ErickNetto <102562548+ErickNetto@users.noreply.github.com>
2022-10-18 17:51:53 -0300

    é isso
    nao deu pra fazer muita coisa pq tá complicadinho, mas aos poucos vai, próxima coisa a fazer é terminar as outras 2 manchetes de cima e logo após fazer as colunas de notícias

--- index.php
+++ index.php
@@ -3,15 +3,21 @@
 include_once './includes/_header.php'
 //parte do conteudo da página
 ?>
-<main>
+<main class="container-fluid">
     <div class='row'>
         <div class="col">
-           <section style="">
-                <img src="./assets/img/Rectangle 13.png" style="height:200px" alt="">
-           </section>
-        
-       
-            
+                <section style="display:inline;">
+                    <section class="text_on_thumb">
+                        <img class="thumbnail" src="./assets/img/Rectangle 13.png" style="height:200px" alt="">
+                    
+                        <div class="brightness_off">
+
+                        </div>
+                    </section>
+                    <h1 class="centered_title">Guerra dos Drones</h1>
+                    <p class="centered_text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam ipsam est illo, excepturi, placeat nesciunt sint odit deleniti fuga ratione minima commodi adipisci tempore itaque? Laboriosam eum quibusdam nobis! Placeat.</p>
+                </section>
+                
         </div>
     </div>
 </main>
