5a0b5ee1bc67817a64e23ca59a728750f98b9a23
FelipeMYung <102309048+FelipeMYung@users.noreply.github.com>
2022-10-19 13:25:14 -0300

    Aside final

--- includes/_aside.php
+++ includes/_aside.php
@@ -10,6 +10,14 @@
             <div class="col-4"><img src="https://cdn.create.vista.com/api/media/small/191578550/stock-photo-cropped-shot-robot-businessman-doing" class="img-news" alt=""></div>
             <div class="col-8"><p class="texto-aside">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla rerum libero temporibus nostrum ipsum neque necessitatibus fuga quo sint nihil, maxime fugiat beatae deleniti. Labore corrupti autem a commodi quidem?</p></div>
         </div>
+        <div class="row">
+            <div class="col-4"><img src="https://cdn.create.vista.com/api/media/small/191578550/stock-photo-cropped-shot-robot-businessman-doing" class="img-news" alt=""></div>
+            <div class="col-8"><p class="texto-aside">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla rerum libero temporibus nostrum ipsum neque necessitatibus fuga quo sint nihil, maxime fugiat beatae deleniti. Labore corrupti autem a commodi quidem?</p></div>
+        </div>
+        <div class="row">
+            <div class="col-4"><img src="https://cdn.create.vista.com/api/media/small/191578550/stock-photo-cropped-shot-robot-businessman-doing" class="img-news" alt=""></div>
+            <div class="col-8"><p class="texto-aside">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla rerum libero temporibus nostrum ipsum neque necessitatibus fuga quo sint nihil, maxime fugiat beatae deleniti. Labore corrupti autem a commodi quidem?</p></div>
+        </div>
     </section>
 
     
