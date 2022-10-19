3a5e30187dace5464ba9243d416e75b72c6c6044
Leleonardi <leonardobr5552005@gmail.com>
2022-10-19 14:57:05 -0300

    Commit atualização footer
    atualização 2

--- includes/_footer.php
+++ includes/_footer.php
@@ -1,7 +1,28 @@
-<footer>
-<section class="logo"><img src=""></section>
-<section class="redes"><img src=""><img src=""><img src=""><img src=""><img src=""><img src=""></section>
-<section class="mapa"><img src=""></section>
+<footer class="row">
+    
+    <section id="footer-logo" class="col-lg-2 col-12">
+
+    <img id="footer-img-logo" src="./assets/img/logo-senac.png"  alt="...">
+
+    </section>
+    
+    <section id="footer-redes" class="col-lg-8 col-12">
+
+    <img src="..." class="img-fluid" alt="...">
+    <img src="..." class="img-fluid" alt="...">
+    <img src="..." class="img-fluid" alt="...">
+    <img src="..." class="img-fluid" alt="...">
+    <img src="..." class="img-fluid" alt="...">
+    <img src="..." class="img-fluid" alt="...">
+
+    </section>
+    
+    <section id="footer-mapa" class="col-lg-2 col-12">
+
+    <div class="mapouter"><div class="gmap_canvas"><iframe width="193" height="157" id="gmap_canvas" src="https://maps.google.com/maps?q=Rio%20grande%20do%20sul%20Porto%20alegre%20Shopping%20total&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://123movies-to.org">123movies</a><br><style>.mapouter{position:relative;text-align:right;height:157px;width:193px;}</style><a href="https://www.embedgooglemap.net"></a><style>.gmap_canvas {overflow:hidden;background:none!important;height:157px;width:193px;}</style></div></div>
+
+    </section>
+    
 </footer>
 
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
