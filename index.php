9b3559600d37caf340f004cff4c122035da1b7ba
ErickNetto <102562548+ErickNetto@users.noreply.github.com>
2022-10-19 16:05:43 -0300

    meudeus do ceu
    apaguei uns negocio mas só na branch da main
    
    fiz a parte de cima ali
    agora é terminar a parte de baixo e partir pras outras telas

--- index.php
+++ index.php
@@ -1,22 +1,51 @@
 <?php
 // include de header
-include_once './includes/_header.php'
+//include_once './includes/_header.php'
 //parte do conteudo da página
 ?>
-<main class="col-lg-8">
-    <div class="img-destaque-home">
-        <img src="./assets/img/Rectangle 13.png" alt="">
-    </div>
-    <div>
-        <img src="./assets/img/Rectangle 13.png" alt="">
-    </div>
-    <div>
-        <img src="./assets/img/Rectangle 13.png" alt="">
-    </div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/style.css">
+
+    <title>Hello, world!</title>
+  </head>
+<body class="container-fluid">
+    
+    <main class="col-lg-8 col-12">
+    
+        <div class="row">
+            <div class="col-8 thumb_w_text" >
+                <img src="./assets/img/Rectangle 13.png" style="height:100%; width:100%">
+                <div class="dark"></div>
+                <h1 class="title_for_thumb">Guerra dos Drones</h1>
+                <p class="text_for_thumb">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur aliquid distinctio deserunt molestiae. Provident facere architecto distinctio odio velit id consequuntur corporis reiciendis facilis, necessitatibus similique voluptates, exercitationem recusandae excepturi.</p>
+            </div>
+            <div class="col-4">
+                <div class="row thumb_w_text">
+                    <img src="./assets/img/Rectangle 13.png" style="height:auto;width:100%">
+                    <div class="dark"></div>
+                    <h1 class="title_for_thumb">Lorem</h1>
+                    <p class="text_for_thumb">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus molestiae.</p>
+                </div>
+                <br>
+                <div class="row thumb_w_text">
+                    <img src="./assets/img/Rectangle 13.png" style="height:auto;width:100%">
+                    <div class="dark"></div>
+                    <h1 class="title_for_thumb">Lorem</h1>
+                    <p class="text_for_thumb">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita alias dolor magni dolore repellendus.</p>
+                </div>
+            </div>
+        </div>
+        
+    </main>
+</body>
 <?php
 //include do aside
-include_once './includes/_aside.php';
+//include_once './includes/_aside.php';
 
 //include do footer
-include_once './includes/_footer.php';
+//include_once './includes/_footer.php';
 ?>
