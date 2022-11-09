8b6f805a44b3c9a50b69a39778c0400d78fa6fbf
CrisolKnt <jhrk05@gmail.com>
2022-11-09 17:48:31 -0300

    sdasdsad

--- index.php
+++ index.php
@@ -1,24 +1,10 @@
 <?php
 // include de header
-//include_once './includes/_header.php'
+include_once './includes/_header.php'
 //parte do conteudo da página
 ?>
 <main class="col-lg-8 col-12">
-    <h1>Index<h1>
-</main>
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <link rel="stylesheet" href="./assets/css/style.css">
-
-    <title>Hello, world!</title>
-</head>
-<body class="container-fluid">
     
-    <main class="col-lg-8 col-12">
         <div class="card bg-dark text-white">
         <img src="./assets/img/rectangle 13.png" class="card-img" alt="...">
         <div class="card-img-overlay">
@@ -81,8 +67,8 @@
 
 <?php
 //include do aside
-//include_once './includes/_aside.php';
+include_once './includes/_aside.php';
 
 //include do footer
-//include_once './includes/_footer.php';
+include_once './includes/_footer.php';
 ?>
