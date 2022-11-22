929da906c14ac1e1fe68ee10b070e4ed5294254c
JHRK <jhrk05@gmail.com>
2022-11-22 17:25:38 -0300

    dSAD

---
+++ cronicas.php
@@ -0,0 +1,22 @@
+<?php
+// include de header
+include_once './includes/_header.php'
+//parte do conteudo da página
+?>
+
+<main class="col-lg-8 col-12">
+    
+<div class="card-img-overlay">
+    <a href="cronica-pedro.php">
+            <h5 class="card-title mt-5 col-12">Dia de um desinteressado </h5>
+            <p class="card-text col-12">Autor: Pedro Leonardo Schwanck Leffa</p>
+        </div>
+
+    </main>
+<?php
+//include do aside
+include_once './includes/_aside.php';
+
+//include do footer
+include_once './includes/_footer.php';
+?>
