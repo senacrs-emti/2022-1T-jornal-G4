44ee1c5f83df18fe563ec2eeac8126e2e0843edd
Leleonardi <108826787+Leleonardi@users.noreply.github.com>
2022-11-23 15:14:13 -0300

    Atualização aside
    kek

--- includes/_aside.php
+++ includes/_aside.php
@@ -3,12 +3,12 @@
     <section>
         <h3 class="titulo-destaque-geral mt-5 col-12">ARTIGOS DE OPINIÃO</h3>
         <div class="row">
-            <div class="col-6"><img src="./assets/img/noticia-f.jpg" class="img-news" alt=""></div>
-            <div class="col-6"><h2>APPLE E SUA TÁTICA MANIPULADORA </h2><p class="texto-aside">A maior câmera do mundo do observatório de Vera C. Rubin, na Cordilheira dos Andes, é tão potente que capta poeira lunar... </p></div>
+            <div class="col-6"><img src="./assets/img/Apple.jpg" class="img-news" alt=""></div>
+            <div class="col-6"><h2>APPLE E SUA TÁTICA MANIPULADORA </h2><p class="texto-aside">Nós todos sabemos que Apple é uma empresa que já está a anos no mercado vendendo seus... </p></div>
         </div>
         <div class="row">
-            <div class="col-6"><img src="" class="img-news" alt=""></div>
-            <div class="col-6"><h5>EROTIZAÇÃO PRECOCE </h5><p class="texto-aside">Seja honesto, você já cogitou a compra de uma assistente virtual. Se não, já pensou sobre a sua vida com uma...</p></div>
+            <div class="col-6"><img src="./assets/img/xuxa.jpg" class="img-news" alt=""></div>
+            <div class="col-6"><h5>EROTIZAÇÃO PRECOCE </h5><p class="texto-aside">Observamos de forma exponencial pré-adolescentes e crianças sendo expostas a práticas e pensamentos que só deveriam dizer respeit...</p></div>
         </div>
         <div class="row">
             <div class="col-6"><img src="https://cdn.create.vista.com/api/media/small/191578550/stock-photo-cropped-shot-robot-businessman-doing" class="img-news" alt=""></div>
