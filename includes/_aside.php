287bd0799888d102ba2a821ad4b873b8134ccff9
FelipeMYung <102309048+FelipeMYung@users.noreply.github.com>
2022-11-23 14:11:43 -0300

    reescrevendo o aside

--- includes/_aside.php
+++ includes/_aside.php
@@ -3,16 +3,16 @@
     <section>
         <h3 class="titulo-destaque-geral mt-5">MAIS LIDOS</h3>
         <div class="row">
-            <div class="col-6"><img src="https://cdn.create.vista.com/api/media/small/191578550/stock-photo-cropped-shot-robot-businessman-doing" class="img-news" alt=""></div>
-            <div class="col-6"><h5>Apple faz catastrofe e deixa produtos baratos</h5><p class="texto-aside">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla rerum libero temporibus nostrum ipsum neque necessitatibus fuga quo sint nihil, maxime fugiat beatae deleniti...</p></div>
+            <div class="col-6"><img src="./assets/img/noticia-f.jpg" class="img-news" alt=""></div>
+            <div class="col-6"><h2>A MAIOR CÂMERA DIGITAL DO MUNDO QUE CAPTA ATÉ POEIRA LUNAR </h2><p class="texto-aside">A maior câmera do mundo do observatório de Vera C. Rubin, na Cordilheira dos Andes, é tão potente que capta poeira lunar... </p></div>
+        </div>
+        <div class="row">
+            <div class="col-6"><img src="" class="img-news" alt=""></div>
+            <div class="col-6"><h5>Seja inteligente, não Smart</h5><p class="texto-aside">Seja honesto, você já cogitou a compra de uma assistente virtual. Se não, já pensou sobre a sua vida com uma...</p></div>
         </div>
         <div class="row">
             <div class="col-6"><img src="https://cdn.create.vista.com/api/media/small/191578550/stock-photo-cropped-shot-robot-businessman-doing" class="img-news" alt=""></div>
-            <div class="col-6"><h5>Apple faz catastrofe e deixa produtos baratos</h5><p class="texto-aside">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla rerum libero temporibus nostrum ipsum neque necessitatibus fuga quo sint nihil, maxime fugiat beatae deleniti...</p></div>
-        </div>
-        <div class="row">
-            <div class="col-6"><img src="https://cdn.create.vista.com/api/media/small/191578550/stock-photo-cropped-shot-robot-businessman-doing" class="img-news" alt=""></div>
-            <div class="col-6"> <h5>Apple faz catastrofe e deixa produtos baratos</h5><p class="texto-aside">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla rerum libero temporibus nostrum ipsum neque necessitatibus fuga quo sint nihil, maxime fugiat beatae deleniti...</p></div>
+            <div class="col-6"> <h5>A TECNOLOGIA REVOLUCIONÁRIA ALAVANCA A INDUSTRIA AGROPECUARIA A NIVEIS INIMAGINAVEIS </h5><p class="texto-aside">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla rerum libero temporibus nostrum ipsum neque necessitatibus fuga quo sint nihil, maxime fugiat beatae deleniti...</p></div>
         </div>
         <div class="row">
             <div class="col-6"><img src="https://cdn.create.vista.com/api/media/small/191578550/stock-photo-cropped-shot-robot-businessman-doing" class="img-news" alt=""></div>
