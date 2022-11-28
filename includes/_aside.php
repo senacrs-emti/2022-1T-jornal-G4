38a13e818ec4476a58c0470f45c4dfe3243d25c3
Felipe Martins Yung <102309048+FelipeMYung@users.noreply.github.com>
2022-11-28 11:45:43 -0300

    colocando os links no aside

--- includes/_aside.php
+++ includes/_aside.php
@@ -3,20 +3,20 @@
     <section>
         <h3 class="titulo-destaque-geral mt-5 col-12">ARTIGOS DE OPINIÃO</h3>
         <div class="row">
-            <div class="col-6"><img src="./assets/img/Apple.jpg" class="img-news" alt=""></div>
-            <div class="col-6"><h2>APPLE E SUA TÁTICA MANIPULADORA </h2><p class="texto-aside">Nós todos sabemos que Apple é uma empresa que já está a anos no mercado vendendo seus... </p></div>
+            <div class="col-6"><a href="Apple-e-sua-tática-manipuladora.php"><img src="./assets/img/Apple.jpg" class="img-news" alt=""></a></div>
+            <div class="col-6"><a href="Apple-e-sua-tática-manipuladora.php"><h2>APPLE E SUA TÁTICA MANIPULADORA </h2><p class="texto-aside">Nós todos sabemos que Apple é uma empresa que já está a anos no mercado vendendo seus... </p></a></div>
         </div>
         <div class="row">
-            <div class="col-6"><img src="./assets/img/xuxa.jpg" class="img-news" alt=""></div>
-            <div class="col-6"><h5>EROTIZAÇÃO PRECOCE </h5><p class="texto-aside">Observamos de forma exponencial pré-adolescentes e crianças sendo expostas a práticas e pensamentos que só deveriam dizer respeit...</p></div>
+            <div class="col-6"><a href="artigo-erotização.php"><img src="./assets/img/xuxa.jpg" class="img-news" alt=""></a></div>
+            <div class="col-6"><a href="artigo-erotização.php"><h2>EROTIZAÇÃO PRECOCE </h5><p class="texto-aside">Observamos de forma exponencial pré-adolescentes e crianças sendo expostas a práticas e pensamentos que só deveriam dizer respeit...</p></a></div>
+        </div>
+        <div class="row">
+            <div class="col-6"><a href="artigo-smart.php"><img src="https://cdn.create.vista.com/api/media/small/191578550/stock-photo-cropped-shot-robot-businessman-doing" class="img-news" alt=""></a></div>
+            <div class="col-6"><a href="artigo-smart.php"><h2>SEJA INTELIGENTE, NÃO SMART. </h5><p class="texto-aside">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla rerum libero temporibus nostrum ipsum neque necessitatibus fuga quo sint nihil, maxime fugiat beatae deleniti...</p></a></div>
         </div>
         <div class="row">
             <div class="col-6"><img src="https://cdn.create.vista.com/api/media/small/191578550/stock-photo-cropped-shot-robot-businessman-doing" class="img-news" alt=""></div>
-            <div class="col-6"> <h5>SEJA INTELIGENTE, NÃO SMART. </h5><p class="texto-aside">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla rerum libero temporibus nostrum ipsum neque necessitatibus fuga quo sint nihil, maxime fugiat beatae deleniti...</p></div>
-        </div>
-        <div class="row">
-            <div class="col-6"><img src="https://cdn.create.vista.com/api/media/small/191578550/stock-photo-cropped-shot-robot-businessman-doing" class="img-news" alt=""></div>
-            <div class="col-6"><h5>A IMPORTÂNCIA DO DIGITAL INFLUENCER</h5><p class="texto-aside">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla rerum libero temporibus nostrum ipsum neque necessitatibus fuga quo sint nihil, maxime fugiat beatae deleniti...</p></div>
+            <div class="col-6"><h2>A IMPORTÂNCIA DO DIGITAL INFLUENCER</h5><p class="texto-aside">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla rerum libero temporibus nostrum ipsum neque necessitatibus fuga quo sint nihil, maxime fugiat beatae deleniti...</p></div>
         </div>
 </section>
 </aside>
